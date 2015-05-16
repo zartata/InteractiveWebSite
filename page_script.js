c246ec97a60118d190d8c911da8595457cf471e6
Qwurticus <qwurty2.0@gmail.com>
2015-05-15 22:12:54 -0500

    Added HTML5, CSS3, and Jquery-1.11.3 files for the website.

---
+++ page_script.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+	// Get the width and height of the browser window.
+	var windowW = $(window).width();
+	var windowH = $(window).height();
+	
+	// Set up the page_container div so that it has 5px of padding on all sides.
+	$('#page_container').width(windowW - 10);
+	$('#page_container').height(windowH - 10);
+	$('#page_container').css('padding', 5);
+});
