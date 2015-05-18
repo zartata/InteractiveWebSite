32aab48565a00ccc1a1131db23efc7b662083f80
Qwurticus <qwurty2.0@gmail.com>
2015-05-17 19:06:25 -0500

    Added boxes to show basic layout.

--- page_script.js
+++ page_script.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
 	var windowH = $(window).height();
 	
 	// Set up the page_container div so that it has 5px of padding on all sides.
-	$('#page_container').width(windowW - 10);
+	$('#page_container').width(windowW - 12);
 	$('#page_container').height(windowH - 10);
 	$('#page_container').css('padding', 5);
 });
