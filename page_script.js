0dac04997ce815e1daf01b98ea5eb10772bb83a9
Qwurticus <qwurty2.0@gmail.com>
2015-05-18 22:35:46 -0500

    Added tab switching functionality. Added textarea for HTML, CSS, and Javascript 'pages'. Added boilerplate code for HTML and CSS pages.

--- page_script.js
+++ page_script.js
@@ -7,4 +7,76 @@ $(document).ready(function() {
 	$('#page_container').width(windowW - 12);
 	$('#page_container').height(windowH - 10);
 	$('#page_container').css('padding', 5);
+	
+	// Set the height of the editing area to fit under the tab container.
+	$('#editing_area').height($('#editor').height() - 31);
+	
+	// Set the height and width of the HTML, CSS, and JavaScript "pages"
+	// to compensate for the 12px of "padding" on all sides set by CSS.
+	$('.editor_textarea').width($('.editing_slab').width() - 24);
+	$('.editor_textarea').height($('.editing_slab').height() - 24);
+	
+	// Change tab stylings when HTML tab is clicked.
+	$('#html_tab').click(function() {
+		// Change HTML tab.
+		$('#html_tab').css('border-bottom', '1px solid white');
+		$('#html_tab').css('color', 'rgba(0, 0, 255, 1)');
+		$('#html_tab').css('font-style', 'italic');
+		// Change CSS tab.
+		$('#css_tab').css('border-bottom', '1px solid rgba(150, 0, 0, 0.6)');
+		$('#css_tab').css('color', 'rgba(0, 150, 0, 0.8)');
+		$('#css_tab').css('font-style', 'normal');
+		// Change JavaScript tab.
+		$('#javascript_tab').css('border-bottom', '1px solid rgba(150, 0, 0, 0.6)');
+		$('#javascript_tab').css('color', 'rgba(150, 0, 0, 0.8)');
+		$('#javascript_tab').css('font-style', 'normal');
+		// Move the HTML page to the top and the others below.
+		$('#editing_area_html').css('z-index','10');
+		$('#editing_area_css').css('z-index','9');
+		$('#editing_area_javascript').css('z-index','8');
+		// Focus on the HTML textarea.
+		$('#html_textarea').focus();
+	});
+	// Change tab stylings when CSS tab is clicked.
+	$('#css_tab').click(function() {
+		// Change CSS tab.
+		$('#css_tab').css('border-bottom', '1px solid white');
+		$('#css_tab').css('color', 'rgba(0, 200, 0, 1)');
+		$('#css_tab').css('font-style', 'italic');
+		// Change HTML tab.
+		$('#html_tab').css('border-bottom', '1px solid rgba(150, 0, 0, 0.6)');
+		$('#html_tab').css('color', 'rgba(0, 0, 150, 0.8)');
+		$('#html_tab').css('font-style', 'normal');
+		// Change JavaScript tab.
+		$('#javascript_tab').css('border-bottom', '1px solid rgba(150, 0, 0, 0.6)');
+		$('#javascript_tab').css('color', 'rgba(150, 0, 0, 0.8)');
+		$('#javascript_tab').css('font-style', 'normal');
+		// Move the CSS page to the top and the others below.
+		$('#editing_area_html').css('z-index','8');
+		$('#editing_area_css').css('z-index','10');
+		$('#editing_area_javascript').css('z-index','9');
+		// Focus on the CSS textarea.
+		$('#css_textarea').focus();
+	});
+	// Change tab stylings when JavaScript tab is clicked.
+	$('#javascript_tab').click(function() {
+		// Change JavaScript tab.
+		$('#javascript_tab').css('border-bottom', '1px solid white');
+		$('#javascript_tab').css('color', 'rgba(225, 0, 0, 1)');
+		$('#javascript_tab').css('font-style', 'italic');
+		// Change HTML tab.
+		$('#html_tab').css('border-bottom', '1px solid rgba(150, 0, 0, 0.6)');
+		$('#html_tab').css('color', 'rgba(0, 0, 150, 0.8)');
+		$('#html_tab').css('font-style', 'normal');
+		// Change CSS tab.
+		$('#css_tab').css('border-bottom', '1px solid rgba(150, 0, 0, 0.6)');
+		$('#css_tab').css('color', 'rgba(0, 150, 0, 0.8)');
+		$('#css_tab').css('font-style', 'normal');
+		// Move the JavaScript page to the top and the others below.
+		$('#editing_area_html').css('z-index','9');
+		$('#editing_area_css').css('z-index','8');
+		$('#editing_area_javascript').css('z-index','10');
+		// Focus on the JavaScript textarea.
+		$('#javascript_textarea').focus();
+	});
 });
