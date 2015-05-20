1c661593799fd46ffab9b6f40638cda78fff87bf
Qwurticus <qwurty2.0@gmail.com>
2015-05-19 23:01:08 -0500

    Replaced textarea with contenteditable div's in order to allow syntax highlighting (can not use html elements in textarea so spans would not work).

--- page_script.js
+++ page_script.js
@@ -9,13 +9,27 @@ $(document).ready(function() {
 	$('#page_container').css('padding', 5);
 	
 	// Set up the width of the header to the right of the logo to add 5px of space on the logo's right.
-	$('#header_options_container').width($('#header_bar').width() - 55);
-	$('#header_options_container').css('left', 55);
+	$('#header_options_container').width($('#header_bar').width() - 62);
+	$('#header_options_container').css('left', 62);
 	$('#header_options_container').css('top', -53);
-	
 	
 	// Set the height of the editing area to fit under the tab container.
 	$('#editing_area').height($('#editor').height() - 31);
+	
+	// Disable spellcheck for the HTML textarea.
+	// Declaring spellcheck='false' in the HTML will only disable the spellcheck
+	// once that contenteditable div is focused, so do it with JavaScript to do it "immediately".
+	$('#html_textarea')[0].spellcheck = false;
+	$('#html_textarea')[0].focus();
+	$('#html_textarea')[0].blur();
+	// Disable spellcheck for CSS textarea.
+	$('#css_textarea')[0].spellcheck = false;
+	$('#css_textarea')[0].focus();
+	$('#css_textarea')[0].blur();
+	// Disable spellcheck for JavaScript textarea.
+	$('#javascript_textarea')[0].spellcheck = false;
+	$('#javascript_textarea')[0].focus();
+	$('#javascript_textarea')[0].blur();
 	
 	// Set the height and width of the HTML, CSS, and JavaScript "pages"
 	// to compensate for the 12px of "padding" on all sides set by CSS.
@@ -85,5 +99,11 @@ $(document).ready(function() {
 		// Focus on the JavaScript textarea.
 		$('#javascript_textarea').focus();
 	});
+	
+	function parseAndUpdateHTMLEditor()
+	{
+		var htmlText = $('#html_textarea')[0].value;
+		var lowercaseHtmlText = htmlText.toLowerCase();
+	}
 	
 });
